f0502e9c9036420fe84deaf83f26061c731905e4
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-12 20:01:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,4 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 return playlist
 }
 
-function updatePlaylist(playlist, artistName, songTitle){
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist
+}
